df9be32ad2f7bbb81164eba8d612b37cf10e4569
carcent <carli.centurion@gmail.com>
2024-09-19 09:32:02 -0300

    core requirement 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?");
+        string response = Console.ReadLine();
+        int grade = int.Parse(response);
+        
+        if (grade >= 90)
+        {
+        Console.WriteLine("A");
+        }
+        else if (grade >= 80)
+        {
+        Console.WriteLine("B");
+        }
+        else if (grade >= 70)
+        {
+        Console.WriteLine("C");
+        }
+        else if (grade >= 60)
+        {
+        Console.WriteLine("D");
+        }
+        else if (grade < 60)
+        {
+        Console.WriteLine("F");
+        }
     }
 }
