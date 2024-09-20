bca545b95d43348d5a17e0d5a482f13bc06f620b
carcent <carli.centurion@gmail.com>
2024-09-20 08:41:57 -0300

    2nd core requirement

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,32 +1,54 @@
 using System;
+using System.Diagnostics;
 
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
 class Program
 {
     static void Main(string[] args)
     {
         Console.Write("What is your grade percentage?");
+
         string response = Console.ReadLine();
+        
         int grade = int.Parse(response);
         
         if (grade >= 90)
+        
         {
-        Console.WriteLine("A");
+            Console.WriteLine("A");
         }
+        
         else if (grade >= 80)
+        
         {
-        Console.WriteLine("B");
+            Console.WriteLine("B");
         }
+        
         else if (grade >= 70)
+        
         {
-        Console.WriteLine("C");
+            Console.WriteLine("C");
         }
+        
         else if (grade >= 60)
+        
         {
-        Console.WriteLine("D");
+            Console.WriteLine("D");
         }
-        else if (grade < 60)
+        
+        else 
+        
+        {        
+            Console.WriteLine("F");
+        }
+
+        if (grade <= 70)
         {
-        Console.WriteLine("F");
+            Console.WriteLine("You didn't meet the requirements to pass the class. Don't let one fall determine your life! You'll do great next time");
+        }
+        else
+        {
+            Console.WriteLine("Congratulations! You have pass your course. with effort everything is possible");
         }
     }
 }
