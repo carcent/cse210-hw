10af047d3e8841ec568ceebe9a3f8c43419c051a
carcent <carli.centurion@gmail.com>
2024-09-20 09:23:58 -0300

    core requirement 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number?");
+        string number = Console.ReadLine();
+        int MagicNumber = int.Parse(number);
+
+        Console.Write("What is your guess?");
+        string answer = Console.ReadLine();
+        int guess = int.Parse(answer);
+
+        if (MagicNumber > guess)
+        {
+            Console.Write("Higher");
+        }
+
+        if (MagicNumber < guess)
+        {
+            Console.Write("Lower");
+        }
+
+        if (MagicNumber == guess)
+        {
+            Console.Write("You guessed it!");
+        }
     }
 }
