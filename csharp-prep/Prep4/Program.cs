5f330eb33829a1718a2e0df6b15b5857b8fc41df
carcent <carli.centurion@gmail.com>
2024-09-21 22:28:41 -0300

    core requirements prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+       
+        Console.WriteLine("Enter a list of numbers, Type 0 when finished.");
+        int number = -1;
+       
+        while (number != 0)
+        {
+            Console.Write("Enter number:");
+            string answer = Console.ReadLine();
+            number = int.Parse(answer);
+        
+            if (number !=0)
+            {
+                numbers.Add(number);
+            }
+        }
+        
+        int sum = 0;
+
+        foreach (int i in numbers)
+        {
+            sum+= i;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int MaxNumber = numbers[0];
+
+        foreach (int i in numbers)
+            {
+                if (i > MaxNumber)
+                {
+                    MaxNumber = i;
+                }
+            }
+        Console.WriteLine($"The max is: {MaxNumber}");
+
+
     }
 }
