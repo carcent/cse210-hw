d5e7f62a96ce63b25e39c4114b9ef02b43f91b8a
carcent <carli.centurion@gmail.com>
2024-10-05 12:06:48 -0300

    develop Week 3

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference reference = new Reference("Alma", 39, 9, 9);
+        Scripture scripture = new Scripture(reference, "Now my son, I would that ye should repent and forsake your sins, and go no more after the lusts of your eyes, but cross yourself in all these things; for except ye do this ye can in nowise inherit the kingdom of God. Oh, remember, and take it upon you, and cross yourself in these things.");
+
+       
+        Console.WriteLine("Hello! We are going to learn a new scripture together!"); 
+        Console.WriteLine("Press 'enter' to hide words from the scripture, if you want to quit press 'Esc'");
+        
+        while (true)
+        {
+            Console.WriteLine(reference.GetDisplayText());
+            Console.WriteLine(scripture.GetDisplayText());
+            var key = Console.ReadKey(true).Key;
+
+            if (key ==ConsoleKey.Enter)
+            {
+                scripture.HideRandomWords(3);
+
+                if( scripture.IsCompletelyHidden())
+                {
+                    Console.Clear();
+                    Console.WriteLine("All words are hidden");
+                    break;
+
+                }
+
+                
+            }
+            else if(key == ConsoleKey.Escape)
+            {
+                break;
+            }
+
+
+        }
+    
+    
     }
 }
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Scripture
+{
+        Reference _reference;
+    private List<Word> _words; 
+    
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+        foreach (var word in text.Split())
+        {
+            _words.Add(new Word(word)); 
+        }
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+        Random rnd = new Random();
+        int count = 0;
+
+        while (count < numberToHide && _words.Count >0)
+        {
+            int index = rnd.Next(_words.Count);
+            if(!_words[index].IsHidden())
+            {
+                _words[index].Hide();
+                count++;
+            }
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string displayText = "";
+        foreach (var word in _words)
+        {
+            displayText += word.GetDisplayText() + " ";
+        }
+        return displayText.Trim();
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (var word in _words)
+        {
+            if (!word.IsHidden())
+                return false;
+        }
+        return true;
+    }
+    
+}
