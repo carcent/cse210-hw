ea4720e72092e008af31a4c4a3618fa3f6ef170e
carcent <carli.centurion@gmail.com>
2024-10-16 23:15:31 -0300

    learning activity w5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,5 +5,18 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning05 World!");
+
+        Assignment assignment = new Assignment("John Doe", "Story homework");
+       Console.WriteLine(assignment.GetSummary());
+       
+
+        MathAssignment m1 = new MathAssignment("Peter", "Math", "8.3", "8-10");
+        Console.WriteLine(m1.GetSummary());
+        Console.WriteLine(m1.GetHomeworkList());
+
+        WritingAssignment w1 = new WritingAssignment("Paul", "The Victorian era", "mm");
+        Console.WriteLine(w1.GetSummary());
+        Console.WriteLine(w1.GetWritingInformation());
+
     }
 }
