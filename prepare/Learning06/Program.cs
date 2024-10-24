12c0ab4b3613d41d8a29dbb6c912bb679dc1f823
carcent <carli.centurion@gmail.com>
2024-10-23 23:28:15 -0300

    activity 6

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 3);
+        shapes.Add(s1);
+        
+        Rectangle r1 = new Rectangle("Blue", 2, 3);
+        shapes.Add(r1);
+
+        Circle c1 = new Circle("Yellow", 4);
+        shapes.Add(c1);
+    
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} has an area of {area}");
+        }
     }
 }
